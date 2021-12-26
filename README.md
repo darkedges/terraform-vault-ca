c3d98eba1d3409d7e9a8c423519d6785eede3022
Nicholas Irving <nirving@darkedges.com>
2021-12-26 16:31:50 +1100

    Updaed

--- README.md
+++ README.md
@@ -1,66 +1,17 @@
-# HashiCorp Vault Certifciate Authority via Terraform
+# terraform-darkedges-ca
 
 ## Deploy Configuration
 
-```dos
-set VAULT_ADDR=https://vault.darkedges.com
-set TF_VAULT_TOKEN=xxxxxxxx
+**Note:** Using `powershell`
+
+## Deploy
+
+```powershell
+$nodeport = kubectl get svc darkedges-vault-ui -o=jsonpath='{.spec.ports[?(@.port==8200)].nodePort}'
+$env:VAULT_ADDR="http://localhost:$nodeport"
+$env:VAULT_TOKEN ="root"
 terraform init
 terraform plan
 terraform apply --auto-approve
-```
-
-## Get AppRole token
-
-```dos
-vault login
-```
-
-```dos
-vault read auth/approle/role/darkedges/role-id
-Key        Value
----        -----
-role_id    zzzzzzzz-zzzz-zzzz-zzzz-zzzzzzzzzzzz
-```
-
-```dos
-vault write -force auth/approle/role/darkedges/secret-id
-Key                   Value
----                   -----
-secret_id             yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyyyyyyy
-secret_id_accessor    xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
-secret_id_ttl         0s
-```
-
-```dos
-curl https://vault.darkedges.com/v1/auth/approle/login --request POST --data "{\"role_id\":\"zzzzzzzz-zzzz-zzzz-zzzz-zzzzzzzzzzzz\",\"secret_id:\", \"yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyyyyyyy\"}'
-{
-    "request_id": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-    "lease_id": "",
-    "renewable": false,
-    "lease_duration": 0,
-    "data": null,
-    "wrap_info": null,
-    "warnings": null,
-    "auth": {
-        "client_token": "xxxxxxxxxxxxxxxxxxxxxxxxxx",
-        "accessor": "xxxxxxxxxxxxxxxxxxxxxxxx",
-        "policies": [
-            "darkedges",
-            "default"
-        ],
-        "token_policies": [
-            "darkedges",
-            "default"
-        ],
-        "metadata": {
-            "role_name": "darkedges"
-        },
-        "lease_duration": 31536000,
-        "renewable": true,
-        "entity_id": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-        "token_type": "service",
-        "orphan": true
-    }
-}
+kubectl apply -f k8s
 ```
--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
 provider "vault" {
 }
 
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "docker-desktop"
+}
+
 resource "vault_mount" "pki_root" {
-  path                      = "darkedges_root"
+  path                      = "darkedges_idam_root"
   type                      = "pki"
   description               = "This is an pki_root mount"
   default_lease_ttl_seconds = "315360000"
@@ -10,54 +15,57 @@ resource "vault_mount" "pki_root" {
 }
 
 resource "vault_mount" "pki_intermediate" {
-  path                      = "darkedges_intermediate"
+  path                      = "darkedges_idam_intermediate"
   type                      = "pki"
   description               = "This is an pki_intermediate mount"
   default_lease_ttl_seconds = "31536000"
   max_lease_ttl_seconds     = "31536000"
 }
 
-resource "vault_pki_secret_backend_config_urls" "darkedges_root_config_urls" {
+resource "vault_pki_secret_backend_config_urls" "darkedges_idam_root_config_urls" {
   backend                 = vault_mount.pki_root.path
   crl_distribution_points = ["https://vault.darkedges.com/v1/${vault_mount.pki_root.path}/crl"]
   issuing_certificates    = ["https://vault.darkedges.com/v1/${vault_mount.pki_root.path}/ca"]
 }
 
-resource "vault_pki_secret_backend_config_urls" "darkedges_intermediate_config_urls" {
+resource "vault_pki_secret_backend_config_urls" "darkedges_idam_intermediate_config_urls" {
   backend                 = vault_mount.pki_intermediate.path
   crl_distribution_points = ["https://vault.darkedges.com/v1/${vault_mount.pki_intermediate.path}/crl"]
   issuing_certificates    = ["https://vault.darkedges.com/v1/${vault_mount.pki_intermediate.path}/ca"]
 }
 
 resource "vault_pki_secret_backend_root_cert" "root" {
+  depends_on           = [vault_mount.pki_root]
   backend              = vault_mount.pki_root.path
   type                 = "internal"
-  common_name          = "DarkEdges Root"
+  common_name          = "Westpac New Zealand CIAM Root"
   ttl                  = "315360000"
   format               = "pem"
   private_key_format   = "der"
   key_type             = "rsa"
   key_bits             = 4096
   exclude_cn_from_sans = true
-  ou                   = "IT"
-  organization         = "DarkEdges"
-  country              = "AU"
-  locality             = "VIC"
+  ou                   = "idam"
+  organization         = "darkedges"
+  country              = "NZ"
+  locality             = "Auckland"
 }
 
 resource "vault_pki_secret_backend_intermediate_cert_request" "intermediate" {
+  depends_on  = [vault_mount.pki_intermediate]
   backend     = vault_mount.pki_intermediate.path
   type        = "internal"
-  common_name = "DarkEdges Intermediate"
+  common_name = "darkedges_idam Intermediate"
 }
 
 resource "vault_pki_secret_backend_root_sign_intermediate" "intermediate" {
+  depends_on           = [vault_pki_secret_backend_intermediate_cert_request.intermediate]
   backend              = vault_mount.pki_root.path
   csr                  = vault_pki_secret_backend_intermediate_cert_request.intermediate.csr
-  common_name          = "DarkEdges Intermediate"
+  common_name          = "Westpac New Zealand CIAM Intermediate"
   exclude_cn_from_sans = true
-  ou                   = "DarkEdges"
-  organization         = "IT"
+  ou                   = "ciam"
+  organization         = "darkedges"
 }
 
 resource "vault_pki_secret_backend_intermediate_set_signed" "intermediate" {
@@ -68,51 +76,64 @@ resource "vault_pki_secret_backend_intermediate_set_signed" "intermediate" {
 resource "vault_pki_secret_backend_role" "admin" {
   backend          = vault_mount.pki_intermediate.path
   name             = "admin"
-  allowed_domains  = ["darkedges.com"]
+  allowed_domains  = [".darkedges.com"]
   allow_subdomains = true
-  key_usage        = ["DigitalSignature", "KeyAgreement", "KeyEncipherment"]
   max_ttl          = "28296000"
+  key_usage        = ["DigitalSignature", "KeyAgreement", "KeyEncipherment"]
 }
 
-resource "vault_pki_secret_backend_role" "darkedges" {
-  backend               = vault_mount.pki_intermediate.path
-  name                  = "darkedges"
-  allow_any_name        = true
-  require_cn            = false
-  use_csr_common_name   = true
-  enforce_hostnames     = false
-  server_flag           = false
-  client_flag           = true
-  code_signing_flag     = false
-  email_protection_flag = false
-  key_bits              = 2048
-  key_type              = "rsa"
-  key_usage             = ["DigitalSignature"]
-  max_ttl               = "28296000"
+resource "vault_pki_secret_backend_role" "darkedges_idam" {
+  backend             = vault_mount.pki_intermediate.path
+  name                = "darkedges_idam"
+  allowed_domains     = [".darkedges.com", "cluster.local"]
+  allow_subdomains    = true
+  allow_glob_domains  = true
+  use_csr_common_name = true
+  require_cn          = false
+  key_usage           = ["DigitalSignature", "KeyAgreement", "KeyEncipherment"]
 }
 
-resource "vault_auth_backend" "approle" {
-  type = "approle"
-
-  tune {
-    default_lease_ttl = "31536000s"
-    max_lease_ttl     = "31536000s"
+data "kubernetes_service_account" "certmanager" {
+  metadata {
+    name = "certmanager"
   }
-
 }
 
-resource "vault_policy" "darkedges" {
-  name = "darkedges"
+data "kubernetes_secret" "certmanager" {
+  metadata {
+    name = data.kubernetes_service_account.certmanager.default_secret_name
+  }
+}
+
+resource "vault_auth_backend" "kubernetes" {
+  type = "kubernetes"
+}
+
+resource "vault_kubernetes_auth_backend_config" "example" {
+  backend                = vault_auth_backend.kubernetes.path
+  kubernetes_host        = "https://10.96.0.1:443"
+  kubernetes_ca_cert     = data.kubernetes_secret.certmanager.data["ca.crt"]
+  token_reviewer_jwt     = data.kubernetes_secret.certmanager.data["token"]
+  issuer                 = "api"
+  disable_iss_validation = "true"
+}
+
+resource "vault_policy" "certmanager-policy" {
+  name = "certmanager-policy"
 
   policy = <<EOT
-path "darkedges_intermediate/*" {
+path "darkedges_idam_intermediate/*" {
   capabilities = ["read","list","delete","update","create"]
 }
 EOT
 }
 
-resource "vault_approle_auth_backend_role" "darkedges" {
-  backend        = vault_auth_backend.approle.path
-  role_name      = "darkedges"
-  token_policies = ["default", "darkedges"]
+resource "vault_kubernetes_auth_backend_role" "certmanager" {
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "certmanager"
+  bound_service_account_names      = ["certmanager", "darkedges-vault-certmanager"]
+  bound_service_account_namespaces = ["default"]
+  token_ttl                        = 3600
+  token_policies                   = ["${vault_policy.certmanager-policy.name}"]
+  audience                         = null
 }
